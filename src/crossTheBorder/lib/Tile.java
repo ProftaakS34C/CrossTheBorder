77f51ef7a9b56f39c097f27451fc5dc7be00ea49
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-09-26 12:41:59 +0200

    Added Tile class

--- src/crossTheBorder/lib/Tile.java
+++ src/crossTheBorder/lib/Tile.java
@@ -1,7 +1,49 @@
 package crossTheBorder.lib;
 
+import java.awt.*;
+
 /**
- * Created by Oscar on 26-Sep-16.
+ *  Represents a single tile that composes the map.
+ *  The tile has a certain location.
+ *  Can have a {@link TileObject} that fills the tile.
  */
 public class Tile {
+    private Point location;
+    private TileObject tileObject;
+
+    /**
+     * Creates a new tile object with the given location.
+     *
+     * @param location The location of the tile.
+     */
+    public Tile(Point location) {
+        this.location = location;
+    }
+
+    /**
+     * Returns whether the tile has a {@link TileObject} or not.
+     *
+     * @return True if the tile has a {@link TileObject}. False if it doesn't have a {@link TileObject}.
+     */
+    public boolean hasTileObject() {
+        return this.tileObject != null;
+    }
+
+    /**
+     * Returns the {@link TileObject} this tile has. Returns null if it does not have a {@link TileObject}.
+     *
+     * @return The {@link TileObject} object. Can be null.
+     */
+    public TileObject getTileObject() {
+        return this.tileObject;
+    }
+
+    /**
+     * Sets the {@link TileObject} for this tile.
+     *
+     * @param tileObject The object that fills the tile.
+     */
+    public void setTileObject(TileObject tileObject) {
+        this.tileObject = tileObject;
+    }
 }
