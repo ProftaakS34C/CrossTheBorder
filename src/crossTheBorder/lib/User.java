a8b68127a0ad24efcc33569e626cd6e7f2349eb9
Joram Tetelepta <joramspam@hotmail.com>
2016-09-26 13:51:06 +0200

    Added User class

--- src/crossTheBorder/lib/User.java
+++ src/crossTheBorder/lib/User.java
@@ -1,7 +1,37 @@
 package crossTheBorder.lib;
 
 /**
- * Created by Oscar on 26-Sep-16.
+ * Represents a user of the game
+ * @author Joram
+ * @version 1.0
  */
 public class User {
+    private String name;
+    private Player player;
+
+    /**
+     * This is the constructor method of the class "User"
+     * in the constructor the name of the player is set
+     * @param name the name of the user
+     */
+    public User(String name, Player player){
+        this.name = name;
+        this.player = player;
+    }
+
+    /**
+     * This method sets the value of player
+     * @param player the player object the user is playing as
+     */
+    public void setPlayer(Player player){
+        this.player = player;
+    }
+
+    /**
+     * This method is used to get the name of the user
+     * @return String the name of the user
+     */
+    public String getName(){
+        return name;
+    }
 }
