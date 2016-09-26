62afd2ff301a4c7e910424f9089128a18ba36e75
Maurice1996 <gamingsessions@hotmail.com>
2016-09-26 12:04:34 +0200

    Class playerentityinitialized

---
+++ src/crossTheBorder/lib/enums/MoveDirection.java
@@ -0,0 +1,7 @@
+package crossTheBorder.lib.enums;
+
+/**
+ * Created by Maurice on 9/26/2016.
+ */
+public enum MoveDirection {
+}
--- src/crossTheBorder/lib/player/PlayerEntity.java
+++ src/crossTheBorder/lib/player/PlayerEntity.java
@@ -1,7 +1,19 @@
 package crossTheBorder.lib.player;
 
+import crossTheBorder.lib.Player;
+
+import java.awt.*;
+
 /**
  * Created by Oscar on 26-Sep-16.
+ * Player entity is the super class for the Mexican and border patrol.
  */
-public class PlayerEntity {
+public class PlayerEntity extends Player {
+    private Point Location;
+
+    public PlayerEntity(String name) {
+        super(name);
+    }
+
+   
 }
