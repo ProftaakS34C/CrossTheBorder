4feff3eabb830688a067ae54a6d1e3404f852fe6
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-09-26 10:59:51 +0200

    Temp ui class

---
+++ src/crossTheBorder/ui/tempName.java
@@ -0,0 +1,18 @@
+package crossTheBorder.ui;/**
+ * Created by Oscar on 26-Sep-16.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class tempName extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
