70578215041067566ade33b18400bc9119fac6ad
Maurice1996 <gamingsessions@hotmail.com>
2016-09-26 11:27:18 +0200

    Class map initialized

--- src/crossTheBorder/lib/Map.java
+++ src/crossTheBorder/lib/Map.java
@@ -4,4 +4,28 @@ package crossTheBorder.lib;
  * Created by Oscar on 26-Sep-16.
  */
 public class Map {
+    private String name;
+    private int width;
+    private int height;
+
+    public Map(String name, int width, int height) {
+        this.name = name;
+        this.width = width;
+        this.height = height;
+    }
+
+    /**
+     * Returns the Width of the map
+     */
+    public int getWidth() {
+        return 0;
+    }
+
+    /**
+     * Returns the height of the map
+     */
+    public int getHeight() {
+        return 0;
+    }
+
 }
