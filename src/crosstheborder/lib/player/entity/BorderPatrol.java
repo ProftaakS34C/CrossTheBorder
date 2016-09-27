c31bff219f130e7b503748512e1da3de05b45ca0
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-09-27 12:13:23 +0200

    Added border patrol class.

--- src/crosstheborder/lib/player/entity/BorderPatrol.java
+++ src/crosstheborder/lib/player/entity/BorderPatrol.java
@@ -1,7 +1,23 @@
 package crosstheborder.lib.player.entity;
 
+import crosstheborder.lib.player.PlayerEntity;
+
 /**
- * Created by Oscar on 26-Sep-16.
+ * This class represents the Border Patrol player object.
+ * Extends the {@link PlayerEntity} class.
+ * @author Joram
+ * @version 1.0
  */
-public class BorderPatrol {
+public class BorderPatrol extends PlayerEntity {
+
+    /**
+     * This is the constructor of the {@link BorderPatrol} class.
+     * <p>
+     * Calls the {@link PlayerEntity#PlayerEntity(String)} constructor.
+     *
+     * @param name The name of the player.
+     */
+    public BorderPatrol(String name) {
+        super(name);
+    }
 }
