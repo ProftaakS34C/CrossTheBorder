f15edf40113c20acf05750949013b57b3322f58f
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-09-27 11:16:33 +0200

    Added Tank class.

--- src/crosstheborder/lib/ability/Tank.java
+++ src/crosstheborder/lib/ability/Tank.java
@@ -1,7 +1,33 @@
 package crosstheborder.lib.ability;
 
+import crosstheborder.lib.Ability;
+
 /**
- * Created by Oscar on 26-Sep-16.
+ * Tank Ability. Unknown ability.
+ * Must override the userAbility method from Ability.
  */
-public class Tank {
+public class Tank extends Ability {
+    /**
+     * This is the constructor method of the class "Ability".
+     * in the constructor the cooldownTime is set and readyToUse is set to true;
+     *
+     * @param cooldownTime the time it takes for the ability to be available again.
+     */
+    public Tank(int cooldownTime) {
+        super(cooldownTime);
+    }
+
+    /**
+     * This method is used when a player activates an ability.
+     * readyToUse is set to false.
+     *
+     * @return boolean returns true if the ability was activated, if not this method returns false.
+     */
+    @Override
+    public boolean useAbility() {
+        if (super.useAbility()) {
+
+        }
+        throw new UnsupportedOperationException();
+    }
 }
