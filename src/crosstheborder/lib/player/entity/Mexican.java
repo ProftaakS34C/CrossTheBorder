49ef6ecc9b51e68468ddcd088b78c7cee9c969ea
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-09-27 12:04:08 +0200

    Added Mexican class.

--- src/crosstheborder/lib/player/entity/Mexican.java
+++ src/crosstheborder/lib/player/entity/Mexican.java
@@ -1,7 +1,53 @@
 package crosstheborder.lib.player.entity;
 
+import crosstheborder.lib.Ability;
+import crosstheborder.lib.player.PlayerEntity;
+
 /**
- * Created by Oscar on 26-Sep-16.
+ * This class represents the Mexican player object.
+ * @author Joram
+ * @version 1.0
  */
-public class Mexican {
+public class Mexican extends PlayerEntity {
+    private boolean isTrapped;
+    private Ability ability;
+
+    /**
+     * This is the constructor method of the class "Mexican".
+     * in the constructor the name of the Mexican is set.
+     *
+     * @param name    The name of the Mexican.
+     * @param ability The ability of the Mexican.
+     */
+    public Mexican(String name, Ability ability) {
+        super(name);
+        this.ability = ability;
+    }
+
+    /**
+     * This method is used to get isTrapped.
+     *
+     * @return The boolean the value of isTrapped.
+     */
+    public boolean getIsTrapped() {
+        return isTrapped;
+    }
+
+    /**
+     * Sets the value of the isTrapped field.
+     *
+     * @param value The new boolean value.
+     */
+    public void setIsTrapped(boolean value) {
+        isTrapped = value;
+    }
+
+    /**
+     * This method is used when a Mexican uses his ability.
+     * the method useAbility of class Ability is called.
+     */
+    public void useAbility() {
+        ability.useAbility();
+    }
+
 }
