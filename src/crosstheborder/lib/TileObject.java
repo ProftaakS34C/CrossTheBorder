8edcdae096623d1fb6fa514f403bd78ff33e0b46
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-10-03 10:34:07 +0200

    Changed TileObject to be an interface.

--- src/crosstheborder/lib/TileObject.java
+++ src/crosstheborder/lib/TileObject.java
@@ -3,30 +3,19 @@ package crosstheborder.lib;
 /**
  * Represents an object on a tile.
  */
-public abstract class TileObject {
-
-    private boolean isPassable;
-
-    /**
-     * Creates a new TileObject.
-     */
-    public TileObject(boolean isPassable) {
-        this.isPassable = isPassable;
-    }
+public interface TileObject {
 
     /**
      * Gets whether this object is passable or not.
      *
      * @return A boolean that represents whether this object is passable.
      */
-    public boolean isPassable() {
-        return isPassable;
-    }
+    boolean isPassable = false;
 
     /**
-     * Method for handling the interaction between a player and a TileObject.
+     * Method for handling the interaction between two {@link TileObject}s.
      *
-     * @param player The player object that is interacting with the object.
+     * @param o The TileObject that is interacting with this object.
      */
-    public abstract void interactWith(Player player);
+    void interactWith(TileObject o);
 }
