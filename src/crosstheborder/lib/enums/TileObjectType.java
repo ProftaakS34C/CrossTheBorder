4f7732d6674fb3b6a77d3a80a928d5241a34d03f
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-10-03 11:15:43 +0200

    Implemented TileObjectType enumeration.

---
+++ src/crosstheborder/lib/enums/TileObjectType.java
@@ -0,0 +1,12 @@
+package crosstheborder.lib.enums;
+
+/**
+ * Enumeration that covers the different classes that implement TileObject.
+ */
+public enum TileObjectType {
+    BorderPatrol,
+    Mexican,
+    Trap,
+    Wall,
+    Obstacle
+}
