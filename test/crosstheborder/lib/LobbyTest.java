c4cedd7f2cf994cab751e2f7422a286bbb98769b
guillaime <guillaimejanssen@msn.com>
2016-10-03 10:07:30 +0200

    Added more tests

---
+++ test/crosstheborder/lib/LobbyTest.java
@@ -0,0 +1,63 @@
+package crosstheborder.lib;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by guill on 3-10-2016.
+ */
+public class LobbyTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getName() throws Exception {
+
+    }
+
+    @Test
+    public void setName() throws Exception {
+
+    }
+
+    @Test
+    public void getPassword() throws Exception {
+
+    }
+
+    @Test
+    public void setPassword() throws Exception {
+
+    }
+
+    @Test
+    public void getMaxPlayers() throws Exception {
+
+    }
+
+    @Test
+    public void setMaxPlayers() throws Exception {
+
+    }
+
+    @Test
+    public void addMessage() throws Exception {
+
+    }
+
+    @Test
+    public void startGame() throws Exception {
+
+    }
+
+}
