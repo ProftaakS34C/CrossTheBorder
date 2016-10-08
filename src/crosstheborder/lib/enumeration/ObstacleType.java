1df9f304f25732680bedc636154372ed52018f81
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-10-08 20:01:01 +0200

    Added an ObstacleType enum to capture different types on obstacles. Made Obstacle obey the new TileObject interface.

---
+++ src/crosstheborder/lib/enumeration/ObstacleType.java
@@ -0,0 +1,11 @@
+package crosstheborder.lib.enumeration;
+
+/**
+ * The ObstacleType class is an enumeration that captures all the different types of obstacles.
+ * Used to determine what sprite to draw on the screen.
+ *
+ * @author Oscar de Leeuw
+ */
+public enum ObstacleType {
+    Tree, Water, Rock
+}
