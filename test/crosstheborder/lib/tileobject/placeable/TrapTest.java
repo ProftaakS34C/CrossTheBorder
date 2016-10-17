eafb6e4cf7f5a7968716b8dc046e0f47c0f09bca
guillaime <guillaimejanssen@msn.com>
2016-10-17 15:18:16 +0200

    Added untitests for the following classes: Trump, Trap, Wall, Obstacle and MapLoader

---
+++ test/crosstheborder/lib/tileobject/placeable/TrapTest.java
@@ -0,0 +1,53 @@
+package crosstheborder.lib.tileobject.placeable;
+
+import crosstheborder.lib.Game;
+import crosstheborder.lib.User;
+import crosstheborder.lib.ability.Crawler;
+import crosstheborder.lib.enumeration.ObstacleType;
+import crosstheborder.lib.enumeration.TileType;
+import crosstheborder.lib.interfaces.GameSettings;
+import crosstheborder.lib.player.Trump;
+import crosstheborder.lib.player.entity.BorderPatrol;
+import crosstheborder.lib.player.entity.Mexican;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by guill on 17-10-2016.
+ */
+public class TrapTest {
+
+    private Game game;
+    private GameSettings settings;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new Game("empty");
+        settings = game.getSettings();
+    }
+
+    @Test
+    public void canPlaceWithNeighbours() throws Exception {
+
+        // First tileObject
+        game.changeTileObjectLocation(new Trap(settings), game.getMap().getTile(5, 11));
+
+        // TileObject @ 4,11. Should be False
+        Assert.assertFalse(new Trap(settings).canPlaceWithNeighbours(
+                game.getMap().getTile(4, 12).getTileObject(),
+                game.getMap().getTile(4, 10).getTileObject(),
+                game.getMap().getTile(3, 11).getTileObject(),
+                game.getMap().getTile(5, 11).getTileObject()));
+
+        // TileObject @ 6,12. Should be True
+        Assert.assertTrue(new Trap(settings).canPlaceWithNeighbours(
+                game.getMap().getTile(6, 13).getTileObject(),
+                game.getMap().getTile(6, 11).getTileObject(),
+                game.getMap().getTile(5, 12).getTileObject(),
+                game.getMap().getTile(7, 12).getTileObject()));
+
+    }
+}
