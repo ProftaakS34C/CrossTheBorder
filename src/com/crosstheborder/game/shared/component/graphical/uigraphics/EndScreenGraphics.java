6495b3bc42158beebba946486aff0eb2bc4fc3a8
Yannic Riesebos <zebybez@surfspot.nu>
2017-01-19 16:23:40 +0100

    Made endscreen graphics.

--- src/com/crosstheborder/game/shared/component/graphical/uigraphics/EndScreenGraphics.java
+++ src/com/crosstheborder/game/shared/component/graphical/uigraphics/EndScreenGraphics.java
@@ -14,17 +14,25 @@ public class EndScreenGraphics extends GraphicsComponent {
     public void render(Object caller, Painter painter, Point location, int width, int height) {
         EndScreen screen = (EndScreen) caller;
 
-        //test
-        painter.drawString("DIT IS EEN TEST", new Point(5, 5), width, height, Color.CYAN, true);
         //draw the victory flag on the screen
         painter.drawImage(screen.getVictoryFlagFile(), location, width, height);
 
         //draw the victory text on the screen
+        Point victoryTextPoint = new Point((width*3)/10, (height*4)/10);
+        painter.drawString(screen.getVictoryText(),victoryTextPoint, (width*6)/10, (height*2)/10, Color.YELLOW, true);
 
         //draw the score on the screen
+        Point scorePoint = new Point((width*2)/10, (height*6)/10);
+        int mexScore = screen.getMexScore();
+        int usaScore = screen.getUsaScore();
+        String score = "Mex: "+mexScore+" VS Usa: "+usaScore;
+        painter.drawString(score, scorePoint, (width*6)/10, (height*2)/10, Color.YELLOW, true);
 
         //draw the continue button on the screen
+        Point buttonPoint = new Point((width*2)/10, (height*7)/10);
 
+        painter.drawRectangle(buttonPoint, (width*4)/10, (height*2)/10, Color.black, false);
+        painter.drawString("continue", new Point(buttonPoint.x, buttonPoint.y + 100), (width*4)/10, (height*2)/10, Color.YELLOW,true);
     }
 
 }
